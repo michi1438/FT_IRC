9b90ce7662deb858511511ed4acc15eec4fbbab7
Robin <robin.godtschalck@hotmail.fr>
2024-04-03 12:50:58 +0200

    poll < kqueue

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@
 /*   By: robin <robin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 16:09:27 by robin             #+#    #+#             */
-/*   Updated: 2024/03/30 15:41:19 by robin            ###   ########.fr       */
+/*   Updated: 2024/04/03 12:43:16 by robin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <sys/event.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
-#include <string>
+#include <iostream>
 #include <fstream>
-#include <vector>
-#include <poll.h> // Pour poll()
 
-#define RED     "\033[31m"
 #define RESET   "\033[0m"
 #define RED     "\033[31m"      // Rouge
 #define GREEN   "\033[32m"      // Vert
@@ -30,104 +30,132 @@
 #define MAGENTA "\033[35m"      // Magenta
 #define CYAN    "\033[36m"      // Cyan
 
-std::string readHtmlFile(const char *filename) {
+#define MAX_EVENTS 64
+#define PORT 8000
+
+std::string readHtmlFile(const char *filename)
+{
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+	{
         return ""; // Ou une erreur appropriée
     }
 
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
-
     return content;
 }
 
 int main() {
-    // Création du socket
-    int listener = socket(AF_INET, SOCK_STREAM, 0);
-    if (listener == -1) {
-        perror("socket");
-        return 1;
+    int kq = kqueue();
+    if (kq == -1)
+	{
+        perror("Error in kqueue");
+        exit(EXIT_FAILURE);
     }
 
-    // Configuration de l'adresse du serveur
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+	{
+        perror("Error in socket");
+        exit(EXIT_FAILURE);
+    }
+
     struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8000); // Port sur lequel le serveur écoute
     server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    // Liaison du socket à l'adresse et au port spécifiés
-    if (bind(listener, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
-        close(listener);
-        return 1;
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+	{
+        perror("Error in bind");
+        exit(EXIT_FAILURE);
     }
 
-    // Mise en mode écoute du socket
-    if (listen(listener, SOMAXCONN) == -1) {
-        perror("listen");
-        close(listener);
-        return 1;
+    if (listen(server_fd, 10) == -1)
+	{
+        perror("Error in listen");
+        exit(EXIT_FAILURE);
     }
 
-    std::cout << YELLOW << "Server listening on port " << ntohs(server_addr.sin_port) << " ..." << RESET << std::endl;
+    struct kevent events[MAX_EVENTS];
+    struct kevent change_event;
+    EV_SET(&change_event, server_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
 
+    if (kevent(kq, &change_event, 1, NULL, 0, NULL) == -1) {
+        perror("Error in kevent");
+        exit(EXIT_FAILURE);
+    }
 
-    std::vector<pollfd> fds(1); // Initialise le tableau de pollfd avec une seule entrée pour le descripteur de fichier du socket d'écoute
-    fds[0].fd = listener;
-    fds[0].events = POLLIN | POLLOUT;
+    std::cout << YELLOW << "Server started. Listening on port " << PORT << RESET << std::endl;
 
-    while (true) {
-        int pollResult = poll(fds.data(), fds.size(), -1); // Attend indéfiniment les événements d'E/S
-
-        if (pollResult == -1) {
-            perror("poll");
-            return 1;
+    while (true)
+	{
+        int num_events = kevent(kq, NULL, 0, events, MAX_EVENTS, NULL);
+        if (num_events == -1)
+		{
+            perror("Error in kevent");
+            exit(EXIT_FAILURE);
         }
 
-        if (fds[0].revents & POLLIN) {
-            // Nouvelle connexion entrante
-            int client_socket = accept(listener, NULL, NULL);
-            if (client_socket == -1) {
-                perror("accept");
-                return 1;
-            }
-            
-            // Ajouter une nouvelle entrée à fds
-            pollfd new_fd;
-            new_fd.fd = client_socket;
-            new_fd.events = POLLIN | POLLOUT;
-            fds.push_back(new_fd);
-        }
-
-        for (size_t i = 1; i < fds.size(); ++i) {
-            if (fds[i].revents & (POLLIN | POLLOUT)) {
-                // Connexion existante a des données à lire ou à écrire
-                int client_socket = fds[i].fd;
-                if (fds[i].revents & POLLIN) {
-                    std::cout << GREEN << "Client connected." << RESET << std::endl;
-
-                    // Réponse HTTP avec le contenu de socket.html
-                    std::string htmlContent = readHtmlFile("socket.html");
-                    std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + htmlContent;
-
-                    if (send(client_socket, response.c_str(), response.size(), 0) == -1) {
-                        perror("send");
-                        close(client_socket);
-                        return 1;
-                    }
+        for (int i = 0; i < num_events; ++i)
+		{
+            if ((int)events[i].ident == server_fd)
+			{
+                // Accept new connection
+                struct sockaddr_in client_addr;
+                socklen_t client_len = sizeof(client_addr);
+                int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+                if (client_fd == -1)
+				{
+                    perror("Error in accept");
+                    exit(EXIT_FAILURE);
                 }
 
-                // Ferme le socket client
-                close(client_socket);
-                std::cout << BLUE << "Response sent." << RESET << std::endl;
+                // Set client socket to non-blocking
+				if (fcntl(client_fd, F_SETFL, O_NONBLOCK) == -1)
+				{
+                    perror("Error in fcntl");
+                    exit(EXIT_FAILURE);
+                }
 
-                // Supprime le descripteur de fichier du tableau fds
-                fds.erase(fds.begin() + i);
+				if (fcntl(client_fd, F_SETFL, FD_CLOEXEC) == -1)
+				{
+                    perror("Error in fcntl");
+                    exit(EXIT_FAILURE);
+                }
+
+                std::cout << GREEN << "New connection accepted" << RESET << std::endl;
+
+                // Add client socket to kqueue
+                EV_SET(&change_event, client_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+                if (kevent(kq, &change_event, 1, NULL, 0, NULL) == -1)
+				{
+                    perror("Error in kevent");
+                    exit(EXIT_FAILURE);
+                }
+            }
+			else
+			{
+                int client_socket = events[i].ident;
+
+				// Réponse HTTP avec le contenu de socket.html
+				std::string htmlContent = readHtmlFile("socket.html");
+				std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + htmlContent;
+
+				if (send(client_socket, response.c_str(), response.size(), 0) == -1)
+				{
+					perror("Error in send");
+					close(client_socket);
+					return 1;
+				}
+                sleep(5);
+				close(client_socket);
+				std::cout << BLUE << "Response sent." << RESET << std::endl;
             }
         }
     }
-
+    close(server_fd);
+    close(kq);
     return 0;
 }
